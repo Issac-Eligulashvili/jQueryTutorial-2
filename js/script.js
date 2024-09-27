3188878b22ab0f588d37f69fac011a2fee8ae5c0
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-09-27 14:34:30 -0400

    done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+     $('.swatch').on('mouseover', function () {
+          $('#product-photo').attr('src', `img/chair-${this.id}@2x.jpg`);
+          $('.swatch').removeClass('selected');
+          $(this).addClass('selected');
+     })
+});
